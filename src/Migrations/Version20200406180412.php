bab16c6c98bf1ee8fb4bd43d9e027f1fdd955aa9
Mariano Gili <marianogili@gmail.com>
2020-04-06 15:09:40 -0300

    Primera migración de esquema y funciona easy_admin

---
+++ src/Migrations/Version20200406180412.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200406180412 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SEQUENCE registro_prestamo_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE registro_prestamo (id INT NOT NULL, prestador VARCHAR(255) NOT NULL, prestatario VARCHAR(255) NOT NULL, objeto VARCHAR(255) NOT NULL, prestamo_desde DATE NOT NULL, prestamo_hasta DATE NOT NULL, fecha_registro TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE registro_prestamo_id_seq CASCADE');
+        $this->addSql('DROP TABLE registro_prestamo');
+    }
+}
